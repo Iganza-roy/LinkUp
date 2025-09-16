cb751967cb231fe51c816584bd0dfdaaeb144ccc
Iganza-roy <iganzaroy55@gmail.com>
2025-09-16 20:53:26 +0300

    feat: implement routing with BrowserRouter and define routes for Auth, Chat, and Profile pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { Button } from '@/components/ui/button';
+import { BrowserRouter } from 'react-router-dom';
+import Auth from './pages/auth';
+import Chat from './pages/chat';
+import Profile from './pages/profile/profile';
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<Navigate to='/auth' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
