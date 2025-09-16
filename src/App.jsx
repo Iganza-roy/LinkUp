151415e8084103b6ceed9b62b6af1ef3bdbca036
Iganza-roy <iganzaroy55@gmail.com>
2025-09-16 21:28:19 +0300

    feat: implement Login and Register components, update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './pages/auth';
 import Chat from './pages/chat';
 import Profile from './pages/profile/profile';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/auth' element={<Auth />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/chat' element={<Chat />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='*' element={<Navigate to='/auth' />} />
