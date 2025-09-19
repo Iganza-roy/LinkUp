10e3226a0b995751dfbbd7f30389f0908175d7e0
Iganza-roy <iganzaroy55@gmail.com>
2025-09-19 19:04:21 +0300

    feat: update Login component layout and styling, enhance user experience

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,6 @@
 import { FcGoogle } from 'react-icons/fc';
 import logo from '../../assets/logo1.png';
 import { useNavigate } from 'react-router-dom';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
 import { useContext, useState } from 'react';
 import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
 import { toast } from 'sonner';
@@ -41,91 +39,96 @@ const Login = () => {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-[100vh]'>
+    <div className='flex flex-col items-center justify-center md:justify-start min-h-screen pt-7'>
       <img
         src={logo}
         alt='logo'
-        className='w-[130px] cursor-pointer hover:scale-105 transition-all duration-300'
+        className='w-[140px] block mb-2 cursor-pointer hover:scale-105 transition-all duration-300'
       />
-      <div className='bg-[#0D0C36] rounded-[20px] px-5 py-4  justify-center items-center'>
-        <div className='flex flex-col justify-center items-center gap-[20px] my-[10px]'>
+
+      <div className='bg-[#0D0C36] rounded-md px-8 py-8 w-full max-w-[420px] flex flex-col items-center'>
+        <div className='flex flex-col justify-center items-center gap-3 w-full'>
           <div className='text-center'>
-            <h1 className='text-xl font-medium'>Welcome!</h1>
-            <h1 className='text-xl font-medium'>Log In</h1>
+            <h1 className='text-lg font-medium'>Welcome!</h1>
+            <h1 className='text-lg font-medium'>Log In</h1>
           </div>
-          <div className='flex flex-row space-x-3'>
-            <div className='flex flex-row space-x-2 justify-center items-center border border-slate-400 rounded-lg w-[150px] max-[768px]:w-[100px] h-10 p-3 hover:border-blue cursor-pointer'>
-              <FcGoogle className='cursor-pointer text-2xl ' />
-              <span className='text-md hover:text-blue hidden md:block'>
-                Google
-              </span>
-            </div>
-          </div>
-          <div className='ml-4 flex items-center justify-center gap-x-[1.9rem] w-full'>
-            <span className='h-[1px] w-[70px] md:w-full bg-slate-400 block'></span>
-            <p className='text-center md:w-full'>Or Login Using</p>
-            <span className='h-[1px] w-[80px] md:w-full bg-slate-400 block'></span>
+
+          <button
+            type='button'
+            className='flex items-center justify-center gap-2 border border-slate-500 rounded-lg w-[220px] md:w-full h-10 px-3 hover:border-blue'
+          >
+            <FcGoogle className='text-2xl' />
+            <span className='text-md hidden md:block'>Google</span>
+          </button>
+
+          <div className='flex items-center justify-center w-full py-2 mb-6'>
+            <span className='h-[1px] flex-1 bg-slate-400'></span>
+            <p className='text-center font-light text-sm text-slate-400 px-4 whitespace-nowrap'>
+              Or Login Using
+            </p>
+            <span className='h-[1px] flex-1 bg-slate-400'></span>
           </div>
         </div>
-        <div className='p-6 w-full'>
-          <form
-            action=''
-            className='flex flex-col justify-center items-center gap-4 w-[100%]'
-          >
+
+        <form
+          className='flex flex-col justify-center items-center gap-4 w-full'
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            required
+            type='text'
+            placeholder='Email'
+            name='email'
+            onChange={handleChange}
+            className='p-2 rounded-sm bg-slate-50 text-slate-800 w-full'
+          />
+
+          <div className='flex items-center bg-slate-50 rounded-sm w-full py-2 px-3'>
             <input
               required
-              type='text'
-              placeholder='Email'
-              name='email'
+              type={passwordVisible ? 'text' : 'password'}
+              placeholder='Password'
+              name='password'
               onChange={handleChange}
-              className='p-2 rounded-md bg-slate-50 text-dark w-[300px] md:w-full'
+              className='text-slate-800 border-none outline-0 w-full'
             />
-            <div className='flex justify-between items-center bg-slate-50 rounded-md w-[300px] md:w-full py-2 px-[10px] '>
-              <input
-                required
-                type={passwordVisible ? 'text' : 'password'}
-                placeholder='Password'
-                name='password'
-                onChange={handleChange}
-                className='text-dark border-none outline-0 w-full'
+            {passwordVisible ? (
+              <IoMdEye
+                className='text-black text-2xl cursor-pointer'
+                onClick={togglePassword}
               />
-              {passwordVisible ? (
-                <IoMdEye
-                  className='text-dark text-2xl cursor-pointer'
-                  onClick={() => togglePassword()}
-                />
-              ) : (
-                <IoMdEyeOff
-                  className='text-blue text-2xl cursor-pointer'
-                  onClick={() => togglePassword()}
-                />
-              )}
-            </div>
-          </form>
-        </div>
-        <div className='flex flex-col gap-4 justify-center items-center'>
+            ) : (
+              <IoMdEyeOff
+                className='text-[#2563EB] text-2xl cursor-pointer'
+                onClick={togglePassword}
+              />
+            )}
+          </div>
+        </form>
+
+        <div className='flex flex-col gap-3 justify-center items-center w-full mt-4'>
           <p
             onClick={() => navigate('/password-reset')}
-            className='hover:underline hover:text-blue cursor-pointer'
+            className='hover:underline text-sm font-light hover:text-blue cursor-pointer'
           >
             Forgot password?
           </p>
+
           <button
             onClick={handleSubmit}
-            className='bg-blue p-3 w-[140px] max-[768px]:w-[120px] rounded-md transition-all duration-300 hover:scale-105'
+            className='bg-[#2563EB] p-2 w-[160px] rounded-sm transition-all duration-300 hover:scale-105'
           >
             Sign In
           </button>
+
           {err && <p className='text-red-500'>{err}</p>}
+
           <p
-            className='text-lg cursor-pointer'
+            className='text-sm cursor-pointer text-left'
             onClick={() => navigate('/login')}
           >
-            {`Don't Have An Account?`}
-            <a
-              href='/register'
-              className='text-blue underline ml-2 hover:text-pink'
-            >
+            {`Don't Have An Account? `}
+            <a href='/register' className='text-[#2563EB] ml-2 hover:text-pink'>
               Create One
             </a>
           </p>
