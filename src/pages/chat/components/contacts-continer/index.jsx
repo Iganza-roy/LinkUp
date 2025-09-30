0c64839bc911a2d94c1167b35e6d1a6bdf7804ae
Iganza-roy <iganzaroy55@gmail.com>
2025-09-30 21:11:32 +0300

    feat: enhance ContactsContainer layout with logo and titles for Direct Messages and Group

--- src/pages/chat/components/contacts-continer/index.jsx
+++ src/pages/chat/components/contacts-continer/index.jsx
@@ -1,7 +1,30 @@
+import logo1 from '../../../../assets/logo1.png';
 const ContactsContainer = () => {
   return (
-    <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#0D0C36]/58 border-r-2 border-[#0f0e4a] w-full'></div>
+    <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#0D0C36]/58 border-r-2 border-[#0f0e4a] w-full'>
+      <div className='pt-1'>
+        <img className='md:w-[150px] w-[120px]' src={logo1} alt='logo' />
+      </div>
+      <div className='my-5'>
+        <div className='flex items-center justify-between pr-5'>
+          <Title text='Direct Messages' />
+        </div>
+      </div>
+      <div className='my-5'>
+        <div className='flex items-center justify-between pr-5'>
+          <Title text='Group' />
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default ContactsContainer;
+
+const Title = ({ text }) => {
+  return (
+    <h6 className='uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm '>
+      {text}
+    </h6>
+  );
+};
