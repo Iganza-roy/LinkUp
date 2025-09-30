4a9bafb527ce2f19d05b7feee5f8324d59220b98
Iganza-roy <iganzaroy55@gmail.com>
2025-09-30 20:48:22 +0300

    fix: update border color in ContactsContainer component for consistency

--- src/pages/chat/components/contacts-continer/index.jsx
+++ src/pages/chat/components/contacts-continer/index.jsx
@@ -1,6 +1,6 @@
 const ContactsContainer = () => {
   return (
-    <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#0D0C36]/58 border-r-2 border-[#0D0C36] w-full'></div>
+    <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#0D0C36]/58 border-r-2 border-[#0f0e4a] w-full'></div>
   );
 };
 
