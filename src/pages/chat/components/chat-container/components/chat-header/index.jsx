72e0ef6d47de30d94cb98051755e688fb11a0d1b
Iganza-roy <iganzaroy55@gmail.com>
2025-10-08 19:16:31 +0300

    feat: implement layout for ChatHeader component with styling and structure

--- src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -1,5 +1,11 @@
 const ChatHeader = () => {
-  return <div className>header</div>;
+  return <div className='h-[10vh] border-b-2 border-[#] flex items-center justify-between'>
+    <div className='flex gap-5 items-center'>
+      <div></div>
+    </div>
+
+    
+  </div>;
 };
 
 export default ChatHeader;
