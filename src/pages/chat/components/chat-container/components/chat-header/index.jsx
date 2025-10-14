a73c5fbe2c4fff30d4f6e633c14a77266fef0161
Iganza-roy <iganzaroy55@gmail.com>
2025-10-14 13:17:45 +0300

    feat: enhance ChatHeader layout with improved styling and structure

--- src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -1,11 +1,25 @@
+import { getColor } from '../../../../../../lib/utils';
+import { IoClose } from 'react-icons/io5';
 const ChatHeader = () => {
-  return <div className='h-[10vh] border-b-2 border-[#] flex items-center justify-between'>
-    <div className='flex gap-5 items-center'>
-      <div></div>
+  return (
+    <div className='h-15 border-b border-[#34466E] flex items-center'>
+      <div className='flex justify-between items-center w-full px-6 py-10'>
+        <div className='flex items-center gap-3'>
+          <div
+            className={`w-12 h-12 rounded-full ${getColor(
+              2
+            )} flex items-center justify-center text-white font-bold`}
+          >
+            CK
+          </div>
+          <div>
+            <div className='text-white font-semibold'>Chloe Kim</div>
+          </div>
+        </div>
+        <IoClose className='text-lg' />
+      </div>
     </div>
-
-    
-  </div>;
+  );
 };
 
 export default ChatHeader;
